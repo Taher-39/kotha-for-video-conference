e4d65ed2e692e54293b871b2ad3e92755504f84d
Abu Taher <abut70243@gmail.com>
2023-06-10 12:22:06 +0600

    msg

--- src/pages/JoinMeeting.tsx
+++ src/pages/JoinMeeting.tsx
@@ -86,10 +86,13 @@ export default function JoinMeeting() {
   }, [params.id, user, userLoaded, createToast, navigate]);
 
 
+  const appId = 1332313231;
+  const serverSecret = "f284f286399405d029ee24954e37d9f1";
+
   const myMeeting = async (element: any) => {
     const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-      parseInt(process.env.appId!),
-      process.env.serverSecret as string,
+      appId,
+      serverSecret,
       params.id as string,
       user?.uid ? user.uid : generateMeetingID(),
       user?.displayName ? user.displayName : generateMeetingID()
